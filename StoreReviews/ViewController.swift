ff13c6e1c7a6da46a2c25cba4ba1457c2d32592c
ugureratalar <ugur.eratalar@adesso.com.tr>
2020-10-25 13:32:33 +0300

    Initial Commit

---
+++ StoreReviews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreReviews
+//
+//  Created by Ugur Eratalar on 25.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
